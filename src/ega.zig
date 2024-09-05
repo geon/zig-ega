da8f6f6de97998db73afa98a8e362589f5bcf9d0
Victor Widell <victor.widell@divid.se>
2024-09-05 22:04:30 +0200

    Refactor. Just use inline asm.

--- src/ega.zig
+++ src/ega.zig
@@ -1,17 +1,17 @@
-const dpmi = @import("dos").dpmi;
 const ports = @import("ports.zig");
 
 const BIOS_INTERRUPT_VIDEO = 0x10; // The BIOS video interrupt number.
+const SET_CURRENT_VIDEO_MODE = 0x0; // Interrupt service number.
 const VIDEO_MODE_EGA_640_350_16 = 0x10; // Video mode EGA 640 x 350, 16 colors.
 
-fn videoInterrupt(registers: dpmi.RealModeRegisters) void {
-    var regs = registers;
-    dpmi.simulateInterrupt(BIOS_INTERRUPT_VIDEO, &regs);
-}
-
 pub fn setVideoMode() void {
-    // TODO: Add al/ah and ax registers in union with eax.
-    videoInterrupt(.{ .eax = VIDEO_MODE_EGA_640_350_16 });
+    // 0x10 = BIOS_INTERRUPT_VIDEO
+    asm volatile (
+        \\ int $0x10
+        :
+        : [_] "{ah}" (@as(u8, SET_CURRENT_VIDEO_MODE)),
+          [_] "{al}" (@as(u8, VIDEO_MODE_EGA_640_350_16)),
+    );
 }
 
 pub fn waitForFrame() void {
@@ -24,5 +24,13 @@ pub fn waitForFrame() void {
 }
 
 pub fn drawPoint(x: u16, y: u16, color: u8) void {
-    videoInterrupt(.{ .eax = 0x0C00 | @as(u32, @intCast(color)), .ecx = x, .edx = y });
+    // 0x10 = BIOS_INTERRUPT_VIDEO
+    asm volatile (
+        \\ int $0x10
+        :
+        : [_] "{ah}" (@as(u8, 0x0C)),
+          [_] "{al}" (color),
+          [_] "{cx}" (x),
+          [_] "{dx}" (y),
+    );
 }
